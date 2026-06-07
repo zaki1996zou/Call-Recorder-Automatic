64847fe52b3ac7f85b04665f4bbe1f62c8a3c16c
zouhir1996 <moufarijamix50@gmail.com>
2026-06-07 02:59:45 +0100

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.recorder.call"
+    namespace = "com.recorder.callautomatic"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.recorder.call"
+        applicationId = "com.recorder.callautomatic"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/recorder/call/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.recorder.call
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
